8dcaffb0a1d022a1bd0842cb6aa345908f232fdb
IstiakAdil14 <istiakadil346@gmail.com>
2025-11-25 04:10:54 +0600

    Prepare Django for Vercel deployment

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -153,8 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'staticfiles'
 # STATICFILES_DIRS = [
 #     'greatkart/static',
 # ]
@@ -173,11 +174,12 @@ if USE_MINIO:
     STATIC_URL = f"{'https' if MINIO_USE_HTTPS else 'http'}://{MINIO_ENDPOINT}/{MINIO_BUCKET_NAME}/static/"
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     DEFAULT_FILE_STORAGE = "greatkart.media_storages.MediaStorage"
+
 else:
     # Local fallback for development
     STATIC_URL = "/static/"
     STATIC_ROOT = BASE_DIR / "static"
-    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
     DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 STATICFILES_DIRS = [
---
+++ vercel_app.py
@@ -0,0 +1,3 @@
+from vercel_python_django import get_app
+
+app = get_app()
